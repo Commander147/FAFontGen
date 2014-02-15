846dd00d549a0fd6716ea1e86260aa252adae9d9
Joseph Afework <jafework@cirruspath.com>
2014-02-14 16:14:09 -0800

    Initial Commit

---
+++ FontGen/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  FontGen
+//
+//  Created by Joseph Afework on 2/14/14.
+//  Copyright (c) 2014 Cirruspath. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
